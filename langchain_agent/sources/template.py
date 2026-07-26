6dcc7bd9fbb89d8409faba8270df729eb53844fb
Sanskar50 <sanskarsuri50@gmail.com>
2026-07-27 00:20:16 +0530

    minor improvements

--- langchain_agent/sources/template.py
+++ langchain_agent/sources/template.py
@@ -1,11 +1,13 @@
 SUBJECT = "Regarding SDE Opportunity at {company_name}"
 BODY = """Greetings {first_name},
 
-Sanskar here , I am currently working at Needl.ai as a SWE in the Platforms Team in Bangalore and am willing to expand my domain further. I am enthusiastic to work in the Data pipeline or Agentic AI space .
+Sanskar here , I am currently working at Needl.ai as a Software Engineer in the Platforms Team in Bangalore and am willing to expand my domain further. I am enthusiastic to work in the Data pipeline / Backend or Agentic AI space .
 
 I have attached my latest resume for your reference.
 
 Feel free to connect for any queries.
+
+Thank you for your time and consideration.
 
 Best regards,
 Sanskar Suri"""
